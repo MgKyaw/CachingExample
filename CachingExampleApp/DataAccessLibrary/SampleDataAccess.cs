9d0e78f19c6dee91b358263f2536dd6a5226c881
MgKyaw <mgkyaw20112@gmail.com>
2025-01-04 09:26:07 +0700

    feat: Add GetEmployeesCache that set/get data to/from cache

--- CachingExampleApp/DataAccessLibrary/SampleDataAccess.cs
+++ CachingExampleApp/DataAccessLibrary/SampleDataAccess.cs
@@ -1,7 +1,16 @@
-﻿namespace DataAccessLibrary;
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace DataAccessLibrary;
 
 public class SampleDataAccess
 {
+    private readonly IMemoryCache _memoryCache;
+
+    public SampleDataAccess(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
     public List<EmployeeModel> GetEmployees()
     {
         var employees = new List<EmployeeModel>();
@@ -24,6 +33,28 @@ public class SampleDataAccess
         employees.Add(new() { FirstName = "Jane", LastName = "Jones" });
 
         await Task.Delay(3000);
+
+        return employees;
+    }
+
+    public async Task<List<EmployeeModel>> GetEmployeesCache()
+    {
+        List<EmployeeModel> employees;
+
+        employees = _memoryCache.Get<List<EmployeeModel>>("employees");
+
+        if (employees is null)
+        {
+            employees = new();
+
+            employees.Add(new() { FirstName = "Tim", LastName = "Corey" });
+            employees.Add(new() { FirstName = "Sue", LastName = "Storm" });
+            employees.Add(new() { FirstName = "Jane", LastName = "Jones" });
+
+            await Task.Delay(3000);
+
+            _memoryCache.Set("employees", employees, TimeSpan.FromMinutes(1));
+        }
 
         return employees;
     }
