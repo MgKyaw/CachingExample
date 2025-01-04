adc1781dad76c72bf8b5ea0cb2a6afa379332845
MgKyaw <mgkyaw20112@gmail.com>
2025-01-04 08:58:43 +0700

    feat: Add GetEmployeesAsync method

--- CachingExampleApp/DataAccessLibrary/SampleDataAccess.cs
+++ CachingExampleApp/DataAccessLibrary/SampleDataAccess.cs
@@ -14,4 +14,17 @@ public class SampleDataAccess
 
         return employees;
     }
+
+    public async Task<List<EmployeeModel>> GetEmployeesAsync()
+    {
+        var employees = new List<EmployeeModel>();
+
+        employees.Add(new() { FirstName = "Tim", LastName = "Corey" });
+        employees.Add(new() { FirstName = "Sue", LastName = "Storm" });
+        employees.Add(new() { FirstName = "Jane", LastName = "Jones" });
+
+        await Task.Delay(3000);
+
+        return employees;
+    }
 }
