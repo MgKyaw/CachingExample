9630ce23bc79059f7ad8f595549d736415ddfcea
MgKyaw <mgkyaw20112@gmail.com>
2025-01-04 08:34:54 +0700

    feat: Add SampleDataAccess.cs

---
+++ CachingExampleApp/DataAccessLibrary/SampleDataAccess.cs
@@ -0,0 +1,17 @@
+﻿namespace DataAccessLibrary;
+
+public class SampleDataAccess
+{
+    public List<EmployeeModel> GetEmployees()
+    {
+        var employees = new List<EmployeeModel>();
+
+        employees.Add(new() { FirstName = "Tim", LastName = "Corey" });
+        employees.Add(new() { FirstName = "Sue", LastName = "Storm" });
+        employees.Add(new() { FirstName = "Jane", LastName = "Jones" });
+
+        Thread.Sleep(3000);
+
+        return new List<EmployeeModel>();
+    }
+}
